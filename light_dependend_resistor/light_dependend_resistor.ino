2db3677a9dff7bd53beafac0232e9394d8cbfe36
fabiantheblind <icke@fabianmoronzirfas.me>
2013-12-27 12:39:31 +0100

    inital local commit

---
+++ light_dependend_resistor/light_dependend_resistor.ino
@@ -0,0 +1,14 @@
+//light dependent resistor
+
+int LDR_Pin = A0; //analog pin 0
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  int LDRReading = analogRead(LDR_Pin); 
+
+  Serial.println(LDRReading);
+  delay(250); //just here to slow down the output for easier reading
+}
