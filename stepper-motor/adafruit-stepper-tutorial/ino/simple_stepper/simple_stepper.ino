bf2373d3f6baa86e0a39b6e59642cfccb8f6e359
fabianmoronzirfas <icke@fabianmoronzirfas.me>
2017-01-06 14:52:17 +0100

    fix some broken code and clean up

--- stepper-motor/adafruit-stepper-tutorial/ino/simple_stepper/simple_stepper.ino
+++ stepper-motor/adafruit-stepper-tutorial/ino/simple_stepper/simple_stepper.ino
@@ -6,35 +6,23 @@
  *
  * Adafruit Arduino - Lesson 16. Stepper
  */
- 
 #include <Stepper.h>
- 
 int in1Pin = 12;
 int in2Pin = 11;
 int in3Pin = 10;
 int in4Pin = 9;
 int numberOfSteppsPerRevolution = 512;
 Stepper motor(numberOfSteppsPerRevolution, in1Pin, in2Pin, in3Pin, in4Pin);
- 
-void setup()
-{
+void setup(){
   pinMode(in1Pin, OUTPUT);
   pinMode(in2Pin, OUTPUT);
   pinMode(in3Pin, OUTPUT);
   pinMode(in4Pin, OUTPUT);
- 
-  // this line is for Leonardo's, it delays the serial interface
-  // until the terminal window is opened
-  while (!Serial);
-
   Serial.begin(9600);
-  motor.setSpeed(20);
+  motor.setSpeed(50);
 }
- 
-void loop()
-{
-  if (Serial.available())
-  {
+void loop(){
+  if (Serial.available()>0){
     int steps = Serial.parseInt();
     motor.step(steps);
   }
